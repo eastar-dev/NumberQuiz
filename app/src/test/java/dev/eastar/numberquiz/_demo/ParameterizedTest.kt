5df818bff9786ac834b8ba8950c828bac7772bbb
eastar <pluto@eastandroid.com>
2021-04-07 13:19:35 +0900

    ParameterizedTestDemo add

---
+++ app/src/test/java/dev/eastar/numberquiz/_demo/ParameterizedTest.kt
@@ -0,0 +1,15 @@
+package dev.eastar.numberquiz._demo
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@Disabled
+class ParameterizedTestDemo {
+    @ParameterizedTest
+    @ValueSource(strings = ["racecar", "radar", "able was I ere I saw elba"])
+    fun palindromes(candidate: String) {
+        Assertions.assertTrue(candidate.isNotEmpty())
+    }
+}

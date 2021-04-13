3cf9289700c6a5ef99f1caa7027ec3894e71f3ef
eastar <pluto@eastandroid.com>
2021-04-13 17:24:17 +0900

    500-domain impl

--- domain/src/main/java/dev/eastar/domain/GameDomain.kt
+++ domain/src/main/java/dev/eastar/domain/GameDomain.kt
@@ -1,23 +1,20 @@
 package dev.eastar.domain
 
 import android.log.Log
-import androidx.annotation.VisibleForTesting
 
-class GameDomain(val answer: Int) {
+class GameDomain(private val answer: Int, private val player: Array<String> = emptyArray()) {
     private var tryCount: Int = 0
+    private val winner: String
+        get() = player[tryCount / player.size]
+
 
     fun tryNumber(guess: Int): GameResult {
         Log.e(guess, answer)
-
         val result = signumTest(guess, answer)
         val lowHigh = GameResult.values()[result + 1]
         tryCount++
         return lowHigh
-
     }
 
-    @VisibleForTesting
-    fun signumTest(guess: Int, answer: Int): Int {
-        return Integer.signum(guess - answer)
-    }
+    private fun signumTest(guess: Int, answer: Int): Int = Integer.signum(guess - answer)
 }
